e4def5d9b984f0167a3cc615666747cddb59ada0
notusedusername <36472592+notusedusername@users.noreply.github.com>
2019-03-20 07:18:08 +0100

    Update measure_merge.cpp

--- measure_merge.cpp
+++ measure_merge.cpp
@@ -261,23 +261,3 @@ int main(int argc, char* argv[]) {
   cout<<"Legerősebb: "<<*max_element(avgBPS.begin(), avgBPS.end())<<"KB"<<endl;
   return 0;
 }
-/*
-Az R script a program kimenetének feldolgozására:
-
-pdf("udprognum.pdf")
-
-size=c([Ide a div vektor elemei])
-
-plot(size, type="o")
-dev.off()
-
-pdf("udprogmeanFL.pdf")
-
-bps=c([Ide az average vektor elemei])
-names = c("F", "L")
-
-plot(bps, type="o")
-ppp=c(1,3)
-text(bps, names, cex=0.9, font=2, pos=ppp)
-dev.off()
-*/
